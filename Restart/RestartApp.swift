aeeab52aa64858a50adb85d09346b54d80df3b14
Mateo Nieto <matheo.developer@gmail.com>
2023-04-20 15:11:49 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Matheo Nieto on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
