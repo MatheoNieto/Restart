6d01bb4136cca215a0f0695dda4dede297d8ec26
Mateo Nieto <matheo.developer@gmail.com>
2023-04-20 17:47:57 -0500

    render header and center

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,17 +12,71 @@ struct OnboardingView: View {
     
     
     var body: some View {
-        VStack {
-            Text("Onboarding")
-                .font(.largeTitle)
-            
-            Button(action:{
-                isOnboardingViewActive = false
-            })
-            {
-                Text("Start")
-            }
-        }
+        ZStack {
+            Color("primaryColor")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                Spacer()
+
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give
+                    but how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                }//: HEADER
+                
+                // MARK: - CENTER
+                
+                ZStack{
+                    ZStack{
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80 )
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }//: ZSTACK
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+                Spacer()
+                
+                
+                
+                
+                // MARK: - FOOTER
+                ZStack{
+                    // BACKGROUNT (STATIC)
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    // CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack{
+                        Capsule()
+                            .fill(Color("secondaryColor"))
+                            .frame(width: 80)
+                    }
+                    
+                }// : FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
+            } //: VSTACK
+        } //: ZSTACK
     }
 }
 
