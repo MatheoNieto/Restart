746d99316a2aa7849ba75ad434227462d0eae505
Mateo Nieto <matheo.developer@gmail.com>
2023-04-28 17:58:23 -0500

    onboarding screen

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -64,12 +64,38 @@ struct OnboardingView: View {
                     Capsule()
                         .fill(Color.white.opacity(0.2))
                         .padding(8)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x:20)
+                    
                     // CAPSULE (DYNAMIC WIDTH)
                     
                     HStack{
                         Capsule()
                             .fill(Color("secondaryColor"))
                             .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack{
+                        ZStack {
+                            Circle().fill(Color("secondaryColor"))
+                            Circle().fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                            .frame(width: 80, height: 80, alignment: .center)
+                            .onTapGesture {
+                                isOnboardingViewActive = false
+                            }
+                        
+                        Spacer()
                     }
                     
                 }// : FOOTER
